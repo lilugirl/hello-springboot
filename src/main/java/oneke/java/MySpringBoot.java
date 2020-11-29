2bcf1fd3e249127e0de90c526bd8f259521db9bd
liuyi <794516454@qq.com>
2020-11-30 00:15:15 +0800

    init

---
+++ src/main/java/oneke/java/MySpringBoot.java
@@ -0,0 +1,10 @@
+package oneke.java;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBoot {
+    public static void main(String[] args){
+        SpringApplication.run(MySpringBoot.class);
+    }
+}
